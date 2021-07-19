097216e594eb5d3e4eda26ccc2c307005444fff0
Jordan P.R <jgit@fmail.co.uk>
2021-07-19 13:39:34 +0100

    bashrc not bash_profile

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ git -C ~/.asdf checkout "$(git -C ~/.asdf describe --abbrev=0 --tags)"
 echo ". $HOME/.asdf/asdf.sh" > .bash_aliases
 echo ". $HOME/.asdf/completions/asdf.bash" > .bash_aliases
 
-. ~/.bash_profile
+. ~/.bashrc
 
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
