379cdb0e10a2e28c53e1c9ab2ac233f4e375863c
Jordan P.R <jgit@fmail.co.uk>
2021-07-19 13:42:29 +0100

    add swap

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 sudo apt install curl git
+
+sudo fallocate -l 10G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo sysctl vm.vfs_cache_pressure=50 && sudo sysctl vm.swappiness=10
+
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 git -C ~/.asdf checkout "$(git -C ~/.asdf describe --abbrev=0 --tags)"
